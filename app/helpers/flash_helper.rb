840440698d72ed15767e384065083abaf02be4cf
John Koht <john@kohactive.com>
2014-11-10 19:51:08 -0600

    use bootstrap classes

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -4,22 +4,11 @@ module FlashHelper
   # ------------------------------------------------------------------------------------------------------
 
   # a robust method to display flash messages
-  def flash_helper keys=[:notice, :alert, :error]
-    # allow keys to be a symbol or an array of symbols
-    keys = [keys] unless keys.kind_of? Array
-
-    # Alias classes, mostly so that a notice can be a success, alert a notice, etc.
-    classes = {
-      notice: "alert-success",
-      alert: "alert-notice",
-      error: "alert-danger"
-    }
-
+  def flash_helper
     # html from each message
     html = ''
     flash.each do |key, msg|
-      css_class = classes[key] || key
-      html = html + content_tag(:div, raw(msg + close_alert_btn), :class => "alert #{css_class}") if keys.index key
+      html = html + content_tag(:div, raw(msg + close_alert_btn), :class => "alert alert-#{key}")
     end
 
     return (content_tag :div, html.html_safe, id: "flash_helper").html_safe
